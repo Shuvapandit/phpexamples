28041f423630d061d458113d2643bfbf3fc7b299
Shuvapandit <chakrabortyshuva216@gmail.com>
2023-02-07 21:04:39 +0600

    commit variables in php

---
+++ variables.php
@@ -0,0 +1,32 @@
+<?php 
+
+$text="Apple ";  //string
+var_dump($text);
+echo("<br>");
+
+
+$x=10;//int
+var_dump($x);
+echo("<br>");
+
+
+$y=10.5; //float
+var_dump($y);
+
+echo("<br>");
+//boolean
+$a = true;
+var_dump($a);
+echo("<br>");
+
+
+
+$b = false;
+var_dump($b);
+
+
+
+
+
+
+?>
