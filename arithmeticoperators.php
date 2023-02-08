314ba7bcf36012f3e090cd1e60dc6b63b0e621d5
Shuvapandit <chakrabortyshuva216@gmail.com>
2023-02-08 19:08:38 +0600

    commit arithmetic operators in php

---
+++ arithmeticoperators.php
@@ -0,0 +1,21 @@
+<?php 
+//Arithmetic Operators
+//addition
+$x=10;
+$y=20;
+echo "The addition is:" .$x+$y ."<br>";
+//subtraction
+echo "The subtraction is:" .$x-$y ."<br>";
+//Multiplication
+echo "The multiplication is:" .$x*$y ."<br>";
+//Division
+echo "The division is:" .$x/$y ."<br>";
+//Modulus
+echo "The modulus is:" .$x%$y ."<br>";
+//Exponentiation
+echo "The exponentiation is:" .$x**$y ."<br>";
+
+
+
+
+?>
