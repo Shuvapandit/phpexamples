93cc1c5cf9baa36dd49f1b1922c4e4632eec55df
Shuvapandit <chakrabortyshuva216@gmail.com>
2023-02-08 19:10:51 +0600

    Commit Assignment operators in php

---
+++ assignmentoperators.php
@@ -0,0 +1,32 @@
+<?php 
+// Assignment Operators
+$i=10;
+echo "i is   equal: $i <br>";
+//addition
+$x=10;
+$x +=200;
+echo "Addition is : $x <br>";
+
+//subtraction
+$w=200;
+$w -=100;
+echo "subraction is : $w <br>";
+//multipliction
+$e=200;
+$e *=100;
+echo "Multiplication is : $e <br>";
+//Division
+$r=200;
+$r /=100;
+echo "Division is : $r <br>";
+//modulus
+$g=200;
+$g %=500;
+echo "Modulus is : $g <br>";
+
+
+
+
+
+
+?>
