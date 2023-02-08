57ba4b099bbc68584fd337ae681ab6b861a3c941
Shuvapandit <chakrabortyshuva216@gmail.com>
2023-02-08 13:31:22 +0600

    commit for loop

---
+++ forloop.php
@@ -0,0 +1,15 @@
+<?php 
+/* $i = 0; - Initialize the loop counter ($i), and set the start value to 0
+$i<= 10; - Continue the loop as long as $i is less than or equal to 10
+$i++ - Increase the loop counter value by 1 for each iteration */
+
+for($i=1;$i<=400;$i++){
+    echo "The number is:-" .$i ."<br>";  
+
+
+}
+
+
+
+
+?>
