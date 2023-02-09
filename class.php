b2c0ca4673df5fb66a7d6d3b85c63b7a0b0d7ac0
Shuvapandit <chakrabortyshuva216@gmail.com>
2023-02-09 21:41:52 +0600

    commit class in php

---
+++ class.php
@@ -0,0 +1,23 @@
+<?php 
+class Fruit{
+//por
+public $name;
+public $color;
+//method
+
+
+}
+
+$fruits1 =new fruit();
+$fruits2 =new fruit();
+$fruits1->name ='apple';
+$fruits1->color ='green';
+$fruits2->name ='Orange';
+echo "$fruits1->name ";
+echo "$fruits1->color <br>";
+echo "$fruits2->name <br>";
+
+
+
+
+
