1a110ec12150967d279201cc6f4eada64c17bcf8
Shuvapandit <chakrabortyshuva216@gmail.com>
2023-02-10 20:47:05 +0600

    commit constructor in php

--- constructor.php
+++ constructor.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* PHP - The __construct Function
 A constructor allows you to initialize an object's properties upon creation of the object.
 
@@ -6,27 +6,26 @@ If you create a __construct() function, PHP will automatically call this functio
 
 Notice that the construct function starts with two underscores (__)!
  */
-class Fruit{
+class Fruit
+{
 
-public $name;
-public $color;
-function __construct($name,$color){
-    $this->name=$name;
-    $this->color=$color;
-
+    public $name;
+    public $color;
+    public $size;
+    function __construct($name, $color, $size)
+    {
+        //if u upadate value u can code this way
+        /*  $this->name=$name; 
+ $this->color=$color; 
+$this->size=$size;  */
+        echo $this->name = $name;
+        echo $this->color = $color;
+        echo $this->size = $size;
+    }
 }
-function get_name(){
-    return $this->name;
-
-}
-function get_color(){
-    return $this->color;
-
-}
-
-}
-$obj =new Fruit("Mango","Green");
-echo $obj->get_name();
-echo $obj->get_color();
-
-?>
+$obj1 = new Fruit("Mango", " is Red,", "packed 1kg");
+//updated value
+/* $obj1->name="JackFruit"; 
+echo $obj1->name;  */
+echo "<br>";
+$obj2 = new Fruit("Apple", " is Green,", "packed 2kg");
